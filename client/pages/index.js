c3edce5f923a84bf41fc8313fec196420dab3191
Dennis00000 <161707811+Dennis00000@users.noreply.github.com>
2025-03-21 18:18:02 +0200

    Updated project structure with new folders and configuration files

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,13 @@
-"use client"
+import { MainLayout } from "../src/layouts"
+import { HomePage } from "../src/components/home/HomePage"
 
-import { useEffect } from "react"
-import { useRouter } from "next/router"
-import Layout from "../src/components/layout/Layout"
-import { useSession } from "next-auth/react"
-import HomePage from "../src/pages/HomePage"
-
-export default function Home() {
-  const { data: session } = useSession()
-  const router = useRouter()
-
-  useEffect(() => {
-    // Optional: Redirect authenticated users to dashboard
-    if (session) {
-      router.push("/dashboard")
-    }
-  }, [session, router])
-
-  return (
-    <Layout>
-      <HomePage />
-    </Layout>
-  )
+const Home = () => {
+  return <HomePage />
 }
+
+Home.getLayout = (page) => {
+  return <MainLayout>{page}</MainLayout>
+}
+
+export default Home
 
